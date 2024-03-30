47797c63a66421e14dc644e398b68703d2e45fc0
dexleycodeinc <159862116+dexleycodeinc@users.noreply.github.com>
2024-03-29 20:21:08 -0700

    Update mainGUI.py

--- mainGUI.py
+++ mainGUI.py
@@ -17,15 +17,29 @@ def start_simulation():
         PLAYER_STATS[x].clear()
 
     # Take information from GUI and insert into PLAYER_STATS
+    # Player 1
     PLAYER_STATS[0].append(player1_combo.current())
     PLAYER_STATS[0].append(player1_entry.get())
+
+    # Player 2
+    PLAYER_STATS[1].append(player2_combo.current())
+    PLAYER_STATS[1].append(player2_entry.get())
+
+    # Player 3
+    PLAYER_STATS[2].append(player3_combo.current())
+    PLAYER_STATS[2].append(player3_entry.get())
+
+    # Player 4
+    PLAYER_STATS[3].append(player4_combo.current())
+    PLAYER_STATS[3].append(player4_entry.get())
+
     print(*PLAYER_STATS)
 
 root = tb.Window(themename="superhero")
 # root = Tk()
 
 root.title("Player Select")
-root.geometry('800x700')
+root.geometry('600x400')
 
 # Crate Label for entering Rank and Level
 rank_label = tb.Label(root, text="Rank", font=("Helvetica", 18))
@@ -39,7 +53,7 @@ player1_label = tb.Label(root, text="Player 1", font=("Helvetica", 18))
 player1_label.grid(row=2, column=0, padx=5)
 
 # Create Combobox for Player 1
-player1_combo = tb.Combobox(root, bootstyle="success", values=JOB_TITLES)
+player1_combo = tb.Combobox(root, state="readonly", bootstyle="success", values=JOB_TITLES)
 player1_combo.grid(row=2, column=1, padx=5)
 
 # Create Entry for Player 2's Level
@@ -51,7 +65,7 @@ player2_label = tb.Label(root, text="Player 2", font=("Helvetica", 18))
 player2_label.grid(row=3, column=0, padx=5)
 
 # Create Combobox for Player 2
-player2_combo = tb.Combobox(root, bootstyle="success", values=JOB_TITLES)
+player2_combo = tb.Combobox(root, state="readonly", bootstyle="success", values=JOB_TITLES)
 player2_combo.grid(row=3, column=1, padx=5)
 
 # Create Entry for Player 2's Level
@@ -63,7 +77,7 @@ player3_label = tb.Label(root, text="Player 3", font=("Helvetica", 18))
 player3_label.grid(row=4, column=0, padx=5)
 
 # Create Combobox for Player 3
-player3_combo = tb.Combobox(root, bootstyle="success", values=JOB_TITLES)
+player3_combo = tb.Combobox(root, state="readonly", bootstyle="success", values=JOB_TITLES)
 player3_combo.grid(row=4, column=1, padx=5)
 
 # Create Entry for Player 3's Level
@@ -75,7 +89,7 @@ player4_label = tb.Label(root, text="Player 4", font=("Helvetica", 18))
 player4_label.grid(row=5, column=0, padx=5)
 
 # Create Combobox for Player 3
-player4_combo = tb.Combobox(root, bootstyle="success", values=JOB_TITLES)
+player4_combo = tb.Combobox(root, state="readonly", bootstyle="success", values=JOB_TITLES)
 player4_combo.grid(row=5, column=1, padx=5)
 
 # Create Entry for Player 4's Level
