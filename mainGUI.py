bdcd28ffbfa54b22708feb733d80aaa9e5720465
dexleycodeinc <159862116+dexleycodeinc@users.noreply.github.com>
2024-03-31 12:08:38 -0700

    Added boss meter to mainGUI.py
    Added in Entry boxes for player's respective boss meter

--- mainGUI.py
+++ mainGUI.py
@@ -22,23 +22,26 @@ def start_simulation():
     # Player 1
     PLAYER_STATS[0].append(player1_combo.current())
     PLAYER_STATS[0].append(player1_entry.get())
+    PLAYER_STATS[0].append(player1_boss.get())
 
     # Player 2
     PLAYER_STATS[1].append(player2_combo.current())
     PLAYER_STATS[1].append(player2_entry.get())
+    PLAYER_STATS[1].append(player2_boss.get())
 
     # Player 3
     PLAYER_STATS[2].append(player3_combo.current())
     PLAYER_STATS[2].append(player3_entry.get())
+    PLAYER_STATS[2].append(player3_boss.get())
 
     # Player 4
     PLAYER_STATS[3].append(player4_combo.current())
     PLAYER_STATS[3].append(player4_entry.get())
+    PLAYER_STATS[3].append(player4_boss.get())
 
     # Validate user input for Entries
-    passed = False
+    passed = True
     for x in range(4):
-
         # Try if Player's level is an int
         check = PLAYER_STATS[x][1]
         try:
@@ -46,27 +49,42 @@ def start_simulation():
         except:
             messagebox.showerror("Player " + str(x + 1) + " Error", "Please enter a number for Player "
                                  + str(x + 1) + ".")
+            passed = False
         else:
             rank = int(PLAYER_STATS[x][0])
             # No negative integers
             if check < 0:
                 messagebox.showerror("Player " + str(x + 1) + " Error", "Player " + str(x + 1)
                                      + " cannot be a negative level.")
-
+                passed = False
             # No larger numbers over 99. Exception is Eggsecutive VP at 999.
             elif rank == 8 and check > 999:
                 messagebox.showerror("Player " + str(x + 1) + " Error", "Player " + str(x + 1)
                                      + " has too high level of a level.")
+                passed = False
             elif rank != 8 and check > 99:
                 messagebox.showerror("Player " + str(x + 1) + " Error", "Player " + str(x + 1)
                                      + " has too high level of a level.")
-            else:
-                passed = True
+                passed = False
+        # Try if player's boss meter is an int
+        check = PLAYER_STATS[x][2]
+        try:
+            check = int(check)
+        except:
+            messagebox.showerror("Player " + str(x + 1) + " Error", "Please enter a number for Player "
+                                 + str(x + 1) + "'s boss meter.")
+            passed = False
+        else:
+            rank = int(PLAYER_STATS[x][2])
+            if rank < 0 or rank > 5:
+                messagebox.showerror("Player " + str(x + 1) + " Error", "Please enter a number between 0 and 5 "
+                                    "for Player " + str(x + 1) + "'s boss meter.")
+                passed = False
 
-    print(*PLAYER_STATS)
     if passed:
-        go = startSimulation(PLAYER_STATS)
-        go.start()
+        print(*PLAYER_STATS)
+        #go = startSimulation(PLAYER_STATS)
+        #go.start()
 
 root = tb.Window(themename="superhero")
 # root = Tk()
@@ -74,12 +92,15 @@ root = tb.Window(themename="superhero")
 root.title("Player Select")
 root.geometry('600x400')
 
-# Crate Label for entering Rank and Level
+# Crate Label for entering Rank and Level and Boss Meter
 rank_label = tb.Label(root, text="Rank", font=("Helvetica", 18))
 rank_label.grid(row=1, column=1, padx=5)
 
 level_label = tb.Label(root, text="Level", font=("Helvetica", 18))
 level_label.grid(row=1, column=2, padx=5)
+
+boss_label = tb.Label(root, text="Boss Meter", font=("Helvetica", 18))
+boss_label.grid(row=1, column=3, padx=5)
 
 # Create Label for Player 1
 player1_label = tb.Label(root, text="Player 1", font=("Helvetica", 18))
@@ -89,9 +110,15 @@ player1_label.grid(row=2, column=0, padx=5)
 player1_combo = tb.Combobox(root, state="readonly", bootstyle="success", values=JOB_TITLES)
 player1_combo.grid(row=2, column=1, padx=5)
 
-# Create Entry for Player 2's Level
+# Create Entry for Player 1's Level
 player1_entry = tb.Entry(root)
+player1_entry.insert(0, 0)
 player1_entry.grid(row=2, column=2, padx=5)
+
+# Create Entry for Player 1's boss meter
+player1_boss = tb.Entry(root)
+player1_boss.insert(0, 0)
+player1_boss.grid(row=2, column=3, padx=5)
 
 # Create Label for Player 2
 player2_label = tb.Label(root, text="Player 2", font=("Helvetica", 18))
@@ -103,7 +130,13 @@ player2_combo.grid(row=3, column=1, padx=5)
 
 # Create Entry for Player 2's Level
 player2_entry = tb.Entry(root)
+player2_entry.insert(0, 0)
 player2_entry.grid(row=3, column=2, padx=5)
+
+# Create Entry for Player 2's boss meter
+player2_boss = tb.Entry(root)
+player2_boss.insert(0, 0)
+player2_boss.grid(row=3, column=3, padx=5)
 
 # Create Label for Player 3
 player3_label = tb.Label(root, text="Player 3", font=("Helvetica", 18))
@@ -115,21 +148,33 @@ player3_combo.grid(row=4, column=1, padx=5)
 
 # Create Entry for Player 3's Level
 player3_entry = tb.Entry(root)
+player3_entry.insert(0, 0)
 player3_entry.grid(row=4, column=2, padx=5)
+
+# Create Entry for Player 3's boss meter
+player3_boss = tb.Entry(root)
+player3_boss.insert(0, 0)
+player3_boss.grid(row=4, column=3, padx=5)
 
 # Create Label for Player 4
 player4_label = tb.Label(root, text="Player 4", font=("Helvetica", 18))
 player4_label.grid(row=5, column=0, padx=5)
 
-# Create Combobox for Player 3
+# Create Combobox for Player 4
 player4_combo = tb.Combobox(root, state="readonly", bootstyle="success", values=JOB_TITLES)
 player4_combo.grid(row=5, column=1, padx=5)
 
 # Create Entry for Player 4's Level
 player4_entry = tb.Entry(root)
+player4_entry.insert(0, 0)
 player4_entry.grid(row=5, column=2, padx=5)
 
-# Set Combobox default
+# Create Entry for Player 4's boss meter
+player4_boss = tb.Entry(root)
+player4_boss.insert(0, 0)
+player4_boss.grid(row=5, column=3, padx=5)
+
+# Set Combobox default for Rank and Boss Meter
 player1_combo.current(0)
 player2_combo.current(0)
 player3_combo.current(0)
