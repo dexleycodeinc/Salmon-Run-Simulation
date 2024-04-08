4d2f7816c81beb2fae94bf96ed7857f5adfbfe6e
dexleycodeinc <159862116+dexleycodeinc@users.noreply.github.com>
2024-04-07 22:17:09 -0700

    Calculate IDV and hazard level in constructor

--- startSimulation.py
+++ startSimulation.py
@@ -1,17 +1,25 @@
 from IDV import *
 
 class startSimulation:
+    # Constructor
     def __init__(self, playerStats, winRates, scaleCount, gameCount):
         self.playerStats = playerStats
         self.winRates = winRates
         self.scaleCount = scaleCount
         self.gameCount = gameCount
+        self.IDV = 0
+        self.hazardLevel = 0
+
+        self.IDV = calculateIDV(self.playerStats)
+        self.hazardLevel = calculateHazardLevel(self.IDV)
+
         print(*self.playerStats)
         print(*self.winRates)
         print(self.scaleCount)
         print(self.gameCount)
+        print("IDV: " + str(self.IDV))
+        print("Hazard level: " + str(self.hazardLevel))
 
 
     def start(self):
-        IDV = calculateIDV(self.playerStats)
-        print(IDV)
+        print('pass')
